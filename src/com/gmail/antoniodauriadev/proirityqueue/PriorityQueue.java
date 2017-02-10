978c772a0cdb296abca1a9b21ddb0812fb9555e2
TonyDauria94 <Antoniodauriadev@gmail.com>
2017-02-11 00:35:23 +0100

    Added prioirity queue's interface.

---
+++ src/com/gmail/antoniodauriadev/proirityqueue/PriorityQueue.java
@@ -0,0 +1,24 @@
+package com.gmail.antoniodauriadev.proirityqueue;
+
+import com.gmail.antoniodauriadev.exceptions.priorityqueue.EmptyPriorityQueueException;
+import com.gmail.antoniodauriadev.exceptions.priorityqueue.InvalidKeyException;
+
+public interface PriorityQueue<K, V> {
+
+    /**@return The priority queue' size.*/
+    public int size();
+
+    /**@return True if the priority queue is empty.*/
+    public boolean isEmpty();
+
+    /**@return An entry with a less key without removing it.*/
+    public Entry<K,V> min() throws EmptyPriorityQueueException;
+
+    /**Inserts a key-value' couple.
+     * @return The creates entry.*/
+    public Entry<K,V> insert(K key, V value) throws InvalidKeyException;
+
+    /**Remove an entry with a less key.
+     * @return Removed entry.*/
+    public Entry<K,V> removeMin() throws EmptyPriorityQueueException;
+}
