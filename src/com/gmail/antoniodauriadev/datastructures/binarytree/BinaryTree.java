bfa0ca8a2885a425368f5c1f1d6ecaa69bff8faa
lumegliola <37658092+lumegliola@users.noreply.github.com>
2018-03-26 13:43:23 +0200

    Update BinaryTree.java

--- src/com/gmail/antoniodauriadev/datastructures/binarytree/BinaryTree.java
+++ src/com/gmail/antoniodauriadev/datastructures/binarytree/BinaryTree.java
@@ -7,16 +7,5 @@ import com.gmail.antoniodauriadev.datastructures.tree.Tree;
 
 public interface BinaryTree<E> extends Tree<E> {
 
-    /**@return Left child.*/
-    Position<E> left(Position<E> position) throws InvalidPositionException, BoundaryViolationException;
-
-    /**@return Right child.*/
-    Position<E> right(Position<E> position) throws InvalidPositionException, BoundaryViolationException;
-
-    /**@return True if the chosen node has a left child.*/
-    boolean hasLeft(Position<E> position) throws InvalidPositionException;
-
-    /**@return True if the chosen node has a right child.*/
-    boolean hasRight(Position<E> position) throws InvalidPositionException;
-
+   
 }
